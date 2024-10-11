8d9e43186b5a15364c0adf011690bb40089c1b0b
Sekina Hussein <slaveovallah@gmail.com>
2024-10-11 23:59:43 +0300

    Create dats2.js

---
+++ constants/dats2.js
@@ -0,0 +1,253 @@
+
+export const departments = [
+    {
+        id : 0,
+        name : "Dermatology",
+        logo : require('../assets/images/dental.png'),
+        doctors : [
+            {
+                name: "Jhon Doe",
+
+            }
+        ]
+
+    },
+    {
+        id : 1,
+        name : "Endocronology",
+        logo : '../assets/images/endocrology.jpg',
+        doctors : [
+            {
+                name: "Jhon Doe",
+                
+            }
+        ]
+
+    },
+    {
+        id : 3,
+        name : "Family Physician",
+        logo : '../assets/images/fam.jpg',
+        doctors : [
+            {
+                name: "Jhon Doe",
+                
+            }
+        ]
+
+    },
+    {
+        id : 4,
+        name : "Gastrology",
+        logo : '../assets/images/gastro.jpg',
+        doctors : [
+            {
+                name: "Jhon Doe",
+                
+            }
+        ]
+
+    },
+    {
+        id : 5,
+        name : "Dermatology",
+        logo : '../assets/images/gastro.jpg',
+        doctors : [
+            {
+                name: "Jhon Doe",
+                
+            }
+        ]
+
+    },
+    {
+        id : 6,
+        name : "Cardiology",
+        logo : '../assets/images/cardio.jpg',
+        doctors : [
+            {
+                name: "Jhon Doe",
+                
+            }
+        ]
+
+    },
+    {
+        id : 7,
+        name : "Gynecology",
+        logo : '../assets/images/gynacology.jpg',
+        doctors : [
+            {
+                name: "Jhon Doe",
+                
+            }
+        ]
+
+    },
+    {
+        id : 8,
+        name : "Oncology",
+        logo : '../assets/images/derma.jpg',
+        doctors : [
+            {
+                name: "Jhon Doe",
+                
+            }
+        ]
+
+    },
+    {
+        id : 9,
+        name : "Neurology",
+        logo : '../assets/images/brain.png',
+        doctors : [
+            {
+                name: "Jhon Doe",
+                
+            }
+        ]
+
+    },
+    {
+        id : 10,
+        name : "Dentist",
+        logo : '../assets/images/dental.png',
+        doctors : [
+            {
+                name: "Jhon Doe",
+                
+            }
+        ]
+
+    },
+    {
+        id : 11,
+        name : "Genetics",
+        logo : '../assets/images/DNA.png',
+        doctors : [
+            {
+                name: "Jhon Doe",
+                
+            }
+        ]
+
+    },
+
+]
+
+export const doctors = [
+    {
+        id : 0,
+        name : "Bellamy Nicholas",
+        avatar : require('../assets/images/doctor.png'),
+        department : "Cardiology",
+        experience : " 10 years",
+        patients : 100,
+        details : "Dr. Bellamy Nicholas is a top specialist at London Bridge Hospital at London. He has achieved several awards and recognition for is contribution and service in his own field. He is available for private consultation. ",
+        phone : '+1234567890',
+        
+        
+
+    },
+    {
+        id : 1,
+        name : "Roger Ferguson",
+        avatar : require('../assets/images/doctor.png'),
+        department : "Embryology",
+        experience : " 10 years",
+        patients : 100,
+        details : "Dr. Roger Ferguson is a top specialist at London Bridge Hospital at London. He has achieved several awards and recognition for is contribution and service in his own field. He is available for private consultation. ",
+        phone : '+1234567890',
+    },
+    {
+        id : 2,
+        name : "Bella Hola",
+        avatar : require('../assets/images/doctor.png'),
+        department : "Dermatology",
+        experience : " 5 years",
+        patients : 10,
+        details : "Dr. Bella Hola is a top specialist at London Bridge Hospital at London. He has achieved several awards and recognition for is contribution and service in his own field. He is available for private consultation. ",
+        phone : '+1234567890',
+    },
+    {
+        id : 3,
+        name : "Chloe Anderson",
+        avatar : require('../assets/images/doctor.png'),
+        department : "Oncology",
+        experience : " 10 years",
+        patients : 100,
+        details : "Dr.Chloe Andersonis a top specialist at London Bridge Hospital at London. He has achieved several awards and recognition for is contribution and service in his own field. He is available for private consultation. ",
+        phone : '+1234567890',
+    },
+    {
+        id : 4,
+        name : "Andy Jefferson",
+        avatar : require('../assets/images/doctor.png'),
+        department : "Gastronomy",
+        experience : " 8 years",
+        patients : 50,
+        details : "Dr. Andy Jeffersons is a top specialist at London Bridge Hospital at London. He has achieved several awards and recognition for is contribution and service in his own field. He is available for private consultation. ",
+        phone : '+1234567890',
+    },
+    {
+        id : 5,
+        name : "Nick Whtherington",
+        avatar : require('../assets/images/doctor.png'),
+        department : "Gynecology",
+        experience : " 10 years",
+        patients : 130,
+        details : "Dr. Nick Whtherington is a top specialist at London Bridge Hospital at London. He has achieved several awards and recognition for is contribution and service in his own field. He is available for private consultation. ",
+        phone : '+1234567890',
+    },
+    {
+        id : 6,
+        name : "Amy Rock",
+        avatar : require('../assets/images/doctor.png'),
+        department : "Neurology",
+        experience : " 10 years",
+        patients : 100,
+        details : "Dr. Amy Rock is a top specialist at London Bridge Hospital at London. He has achieved several awards and recognition for is contribution and service in his own field. He is available for private consultation. ",
+        phone : '+1234567890',
+    },
+
+    {
+        id : 7,
+        name : "Jhon Doe",
+        avatar : require('../assets/images/doctor.png'),
+        department : "Genetics",
+        experience : " 10 years",
+        patients : 100,
+        details : "Dr. Jhon Doe is a top specialist at London Bridge Hospital at London. He has achieved several awards and recognition for is contribution and service in his own field. He is available for private consultation. ",
+        phone : '+1234567890',
+    },
+    {
+        id : 8,
+        name : "Marco Alto",
+        avatar : require('../assets/images/doctor.png'),
+        department : "Cardiology",
+        experience : " 10 years",
+        patients : 100,
+        details : "Dr. Marco Alto is a top specialist at London Bridge Hospital at London. He has achieved several awards and recognition for is contribution and service in his own field. He is available for private consultation. ",
+        phone : '+1234567890',
+    },
+    {
+        id : 9,
+        name : " Pamy Toggler",
+        avatar : require('../assets/images/doctor.png'),
+        department : "Cardiology",
+        experience : " 10 years",
+        patients : 100,
+        details : "Dr. Pamy Toggler is a top specialist at London Bridge Hospital at London. He has achieved several awards and recognition for is contribution and service in his own field. He is available for private consultation. ",
+        phone : '+1234567890',
+    },
+    {
+        id : 10,
+        name : "Valerie Hemsworth",
+        avatar : require('../assets/images/doctor.png'),
+        department : "Cardiology",
+        experience : " 10 years",
+        patients : 100,
+        details : "Dr. Valerie Hemsworthis a top specialist at London Bridge Hospital at London. He has achieved several awards and recognition for is contribution and service in his own field. He is available for private consultation. ",
+        phone : '+1234567890',
+    },
+]
